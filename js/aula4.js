35c2fa363e3eb49ef85dfbd94212614790559e7e
danielrodrigues2019 <47562539+danielrodrigues2019@users.noreply.github.com>
2021-04-27 01:27:42 -0300

    Adicionado via upload os arquivos Js

---
+++ js/aula4.js
@@ -0,0 +1,13 @@
+$(function () {
+  //   $("img").on("error", function () {
+  //     $(this).attr("src", "img/error.png");
+  //   });
+
+//   $("img").on("error", function () {
+//     var imagem = $(this).attr("src");
+//     alert("Imagem indisponivel !");
+//     $(this).attr("src", "img/error.png")
+//   });
+
+    $('img').width($(windows).width()).height($(window).height());
+});
